59f736e46bc054d73cb5960e5ba749f743370413
Clifton Craig <clifton.craig@gmail.com>
2018-05-01 23:19:12 -0700

    Adds ability to delete from list.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,15 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({opacity: 1, transform:'translateY(0)', offset: 1}),
           ]))
         ]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform:'translateY(0)', offset: 0}),
+            style({opacity:.5, transform:'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform:'translateY(-75%)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+
       ])
     ])
   ]
@@ -41,6 +50,10 @@ export class HomeComponent implements OnInit {
     this.weaponList.push(this.weaponText);
     this.updateCount();
     this.weaponText = "Enter weapon name";
+  }
+
+  removeWeapon(i: number) {
+    this.weaponList.splice(i,1);
   }
 
 }
